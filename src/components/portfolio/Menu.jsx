2e96cc247711405154093c64c4d10cb831ae4070
Akshay Soni <akshaysoni460@gmail.com>
2024-03-30 16:27:37 +0530

    Start Building Portfolio

---
+++ src/components/portfolio/Menu.jsx
@@ -0,0 +1,41 @@
+import Work1 from "../../assets/native-jobs.png";
+import Work2 from "../../assets/cryptoverse.png";
+import Work3 from "../../assets/travel.png";
+import Work4 from "../../assets/blog-1.svg";
+
+const Menu = [
+	{
+		id: 1,
+		image: Work4,
+		title: "LeetCode Question Solutions",
+		category: ["Backend", "C++"],
+		url: "https://leetcode.com/iamakshaysoni",
+		repositoryUrl: "https://github.com/imakshaysoni/LeetCode"
+	},
+	{
+		id: 2,
+		image: Work4,
+		title: "Native SLA Monitoring Tool",
+		category: ["Frontend", "VueJs"],
+		url: "https://github.com/imakshaysoni",
+		repositoryUrl: "https://github.com/imakshaysoni/native_sla_frontend_service"
+	},
+	{
+		id: 3,
+		image: Work4,
+		title: "Coming Soon",
+		category: ["Frontend"],
+		url: "https://github.com/imakshaysoni",
+// 		repositoryUrl: "#"
+	},
+	{
+		id: 4,
+		image: Work4,
+		title: "Coming Soon",
+		category: ["Backend"],
+		url: "https://github.com/imakshaysoni",
+// 		repositoryUrl: ""
+	}
+];
+
+export default Menu;
---
+++ src/components/resume/WorkExperience.jsx
@@ -0,0 +1,38 @@
+const WorkExperience = [
+    {
+        id: 1,
+        title: "Senior Software Engineer",
+        company: "Opcito Technologies",
+        yearsActive: "January 2022 - Present",
+        information: [
+            "Implemented Fast API service with Redis queue (Async task) to provide real-time prediction, process files with Vertex AI to help choose right music with 95% accuracy",
+            "Orchestrated the development of high-performance RESTful APIs using FastAPI, resulting in 40% faster response times and 25% increase in concurrent user capacity",
+            "Seamlessly integrated APIs with AWS API Gateway, enhancing system functionality and interoperability",
+            "Created scalable Python app on AWS Lambda with Boto3 (S3, RDS), used AWS EventBridge, Step Functions, CloudWatch for automation, and Docker for deployment and scalability",
+            "Designed microservices app with Python, Flask for REST APIs. Integrated RabbitMQ for message queuing, Celery for async tasks. Used Docker for scalable and agile deployments",
+        ],
+    },
+    {
+        id: 2,
+        title: "Data Engineer",
+        company: "Futurense Technologies",
+        yearsActive: "Aug 2021 - Jan 2022",
+        information: [
+            "Leveraged Apache PySpark for efficient big data processing, utilizing distributed computing to analyze and transform large datasets at scale.",
+            "Enhanced data processing capabilities through PySpark’s functionalities, enabling faster and more insightful data-driven decision making.",
+        ],
+    },
+    {
+        id: 3,
+        title: "Software Engineer",
+        company: "Amdocs SmartOps India",
+        yearsActive: "Nov 2018 - Aug 2021",
+        information: [
+            "Developed a comprehensive SLA Monitoring Tool using Python and Flask, showcasing back-end development expertise",
+            "Designed and implemented RESTful APIs for seamless data exchange and user interaction. Built functionalities for User, SLA, and Account Management, streamlining operations and enhancing user experience.",
+            "Implement web development services to deliver real-time SLA analysis and insightful visualizations, empowering stakeholders with data-driven decision-making.",
+        ],
+    },
+];
+
+export default WorkExperience;
